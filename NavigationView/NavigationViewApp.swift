efcda5a5f8be03ca231b936cced34bcfecd3a20b
farkhod <986532qw@gmail.com>
2021-10-17 10:56:29 +0300

    Initial Commit

---
+++ NavigationView/NavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewApp.swift
+//  NavigationView
+//
+//  Created by Farkhod on 17.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
